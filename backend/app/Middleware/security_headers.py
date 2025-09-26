c6b7cb0f24d6c2cfbb4a0b4d9f26dcfd7a18329f
PietroDanieli <1pie7d@gmail.com>
2025-09-26 12:57:18 +0200

    disable Content-Security-Policy

--- backend/app/Middleware/security_headers.py
+++ backend/app/Middleware/security_headers.py
@@ -11,5 +11,5 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
         response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
         response.headers["X-Content-Type-Options"] = "nosniff"
         response.headers["X-Frame-Options"] = "DENY"
-        response.headers["Content-Security-Policy"] = "default-src 'self'; script-src 'self'; object-src 'none';"
+        #response.headers["Content-Security-Policy"] = "default-src 'self'; script-src 'self'; object-src 'none';"
         return response
