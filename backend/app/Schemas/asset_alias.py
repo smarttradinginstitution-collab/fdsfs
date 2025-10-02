7dd6fd39c57d2022632d7e124dcfdd87ace5b49d
PietroDanieli <1pie7d@gmail.com>
2025-10-02 17:05:09 +0200

    change attributes

--- backend/app/Schemas/asset_alias.py
+++ backend/app/Schemas/asset_alias.py
@@ -23,4 +23,4 @@ class AssetAliasRead(AssetAliasBase):
     id: UUID
 
     class Config:
-        orm_mode = True
+        from_attributes = True
--- backend/app/Schemas/broker_asset_class.py
+++ backend/app/Schemas/broker_asset_class.py
@@ -16,4 +16,4 @@ class BrokerAssetClassRead(BaseModel):
     asset_class_id: uuid.UUID
 
     class Config:
-        orm_mode = True
+        from_attributes = True
