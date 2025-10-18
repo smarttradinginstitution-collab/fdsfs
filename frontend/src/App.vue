eb64c3442c1997d35434c6739b5c6ea0148ec363
Marco <tradevantage@aol.com>
2025-10-18 16:27:15 +0200

    bn

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,7 +18,7 @@ const authStore = useAuthStore();
 // --- Inactivity Timer ---
 const { start: startInactivityTimer, stop: stopInactivityTimer } = useInactivityTimer(
   authStore.logout,
-  600 // 600 secondi di inattività
+  6000 // 600 secondi di inattività
 );
 
 watch(
